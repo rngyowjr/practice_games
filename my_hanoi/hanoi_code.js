d853a4f13f9f1233c14b43e839c897de7a752867
ninja <rngyowjr@gmail.com>
2020-01-28 23:22:24 -0500

    Added valid move function

--- my_hanoi/hanoi_code.js
+++ my_hanoi/hanoi_code.js
@@ -9,12 +9,25 @@ class Game {
 
   promptMove(user, cb) {
     this.print();
-    user.question("Pick a tower to take from", start => {
-      const startTowerIdx = parseInt(start);
-      user.question("Pick a tower to place on", end => {
-        const endTowerIdx = parseInt(end);
-        cb(startTowerIdx, endTowerIdx);
+    user.question("Pick a tower to take from", first => {
+      const firstTowerIdx = parseInt(first);
+      user.question("Pick a tower to place on", second => {
+        const secondTowerIdx = parseInt(second);
+        cb(firstTowerIdx, secondTowerIdx);
       });
     });
+  }
+
+  isValidMove(firstTowerIdx, secondTowerIdx) {
+    const firstTower = this.towers[firstTowerIdx];
+    const secondTower = this.towers[secondTowerIdx];
+
+    if (firstTower.length === 0) {
+      return false;
+    } else if (secondTower.length === 0) {
+      return true;
+    } else {
+      return firstTower[firstTower.length - 1] < secondTower[secondTower.length - 1]
+    }
   }
 }
