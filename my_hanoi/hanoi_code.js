59fb85c5db84fdda8431d096ad30de729ba67aa5
ninja <rngyowjr@gmail.com>
2020-02-05 22:18:16 -0500

    Added a playscript

--- my_hanoi/hanoi_code.js
+++ my_hanoi/hanoi_code.js
@@ -64,3 +64,5 @@ class Game {
     });
   }
 }
+
+module.exports = Game;
---
+++ my_hanoi/hanoi_play_script.js
@@ -0,0 +1,6 @@
+const Game = require('./game.js');
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
