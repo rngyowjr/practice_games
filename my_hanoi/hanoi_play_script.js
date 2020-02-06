b731b7781757d67dc8bc33083f42cee9257565c8
ninja <rngyowjr@gmail.com>
2020-02-05 22:21:29 -0500

    Added replayability

--- my_hanoi/hanoi_play_script.js
+++ my_hanoi/hanoi_play_script.js
@@ -4,3 +4,17 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+let session = new Game();
+session.run(user, finish);
+
+function finish() {
+  user.question("Play again? y or n:", input => {
+    if (input === "y") {
+      session = new Game();
+      session.run(user, finish);
+    } else {
+      user.close();
+    }
+  });
+};
