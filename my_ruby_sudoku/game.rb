47ea247eabffe62e1f2a9abee1a384e6c30c953a
ninja <rngyowjr@gmail.com>
2020-02-14 10:28:10 -0500

    Added position input method

--- my_ruby_sudoku/game.rb
+++ my_ruby_sudoku/game.rb
@@ -9,4 +9,20 @@ class Game
   def initialize(board)
     @board = board
   end
+
+  def ask_pos
+    pos = nil
+    until pos && valid_pos?(pos)
+      puts "Please enter a position on the board (EX: 2,4)"
+      print "> "
+      pos = parse_pos(gets.chomp)
+    end
+    pos
+  end
+
+  def valid_pos?(pos)
+    pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? {|x| x.between?(0, board.size - 1)}
+  end
 end
